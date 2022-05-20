6b90ff72e0276b5b5ed26c73d00ac50160b51d49
Manoj Baishya <manoj.baishya@siemens.com>
2022-05-20 09:07:02 +0530

    Added video to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,13 +2,26 @@
 import Subpage from "../components/Subpage.vue";
 import Carousel from "../components/Carousel.vue";
 
-</script>
+let video_props = {
+    width: "1024px",
+    height: "683px"
+}
 
+</script>
 <template>
-    <Subpage :header=false :isGeneralPage=false>
-        <Carousel />
-    </Subpage>
+    <subpage :header=false :isGeneralPage=false>
+
+        <iframe src="https://player.vimeo.com/video/637403296?h=d238ab3e9b&loop=1" :width="video_props.width"
+            :height="video_props.height" frameborder="0" allow="autoplay; fullscreen; picture-in-picture">
+        </iframe>
+        <!-- <Carousel /> -->
+
+    </subpage>
 </template>
 
 <style scoped>
+iframe {
+    margin: auto;
+    display: block;
+}
 </style>
